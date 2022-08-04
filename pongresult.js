b4666ab4c6a4f5309705f4345ea3aa4323a9387e
Laetitia Gonin <l.gonin@codeur.online>
2022-08-04 13:03:59 +0200

    flouter/déflouter par joueur ou tous les joueurs. Réussi

--- pongresult.js
+++ pongresult.js
@@ -1,6 +1,6 @@
 /*ajouter un résultat de match*/
 function AddElement(event) {
-       {
+  {
     event.preventDefault();
 
     let IdElement = document.getElementById("ajoutrang");
@@ -15,96 +15,97 @@ function AddElement(event) {
     let Idtable = document.getElementById("body");
 
     Idtable.appendChild(ligne);
-    }
-    {
-        event.preventDefault();
-        let IdElement = document.getElementById("ajoutclassementO");
-console.log(IdElement.value);
-
-let ligne = document.createElement('tr');
-let cellule = document.createElement('td');
-
-cellule.innerHTML = IdElement.value;
-ligne.appendChild(cellule);
-
-let Idtable = document.getElementById("body");
-
-Idtable.appendChild(ligne);
-    }
-    {       
-         event.preventDefault();
-let IdElement = document.getElementById("ajoutpoints");
-console.log(IdElement.value);let ligne = document.createElement('tr');
-let cellule = document.createElement('td');
-
-cellule.innerHTML = IdElement.value;
-ligne.appendChild(cellule);
-
-let Idtable = document.getElementById("body");
-
-Idtable.appendChild(ligne);
-    }
-    {
-        event.preventDefault();
-    
-        let IdElement = document.getElementById("ajoutjoueur");
-        console.log(IdElement.value);
-    
-        let ligne = document.createElement('tr');
-        let cellule = document.createElement('td');
-    
-        cellule.innerHTML = IdElement.value;
-        ligne.appendChild(cellule);
-    
-        let Idtable = document.getElementById("body");
-    
-        Idtable.appendChild(ligne);
-        }
-        {
-            event.preventDefault();
-            let IdElement = document.getElementById("ajoutclub");
+  }
+  {
+    event.preventDefault();
+    let IdElement = document.getElementById("ajoutclassementO");
     console.log(IdElement.value);
-    
+
     let ligne = document.createElement('tr');
     let cellule = document.createElement('td');
-    
+
     cellule.innerHTML = IdElement.value;
     ligne.appendChild(cellule);
-    
+
     let Idtable = document.getElementById("body");
-    
+
     Idtable.appendChild(ligne);
-        }
-        {       
-             event.preventDefault();
-    let IdElement = document.getElementById("ajoutpays");
-    console.log(IdElement.value);let ligne = document.createElement('tr');
+  }
+  {
+    event.preventDefault();
+    let IdElement = document.getElementById("ajoutpoints");
+    console.log(IdElement.value); let ligne = document.createElement('tr');
     let cellule = document.createElement('td');
-    
+
     cellule.innerHTML = IdElement.value;
     ligne.appendChild(cellule);
-    
+
     let Idtable = document.getElementById("body");
-    
+
     Idtable.appendChild(ligne);
-        } 
+  }
+  {
+    event.preventDefault();
+
+    let IdElement = document.getElementById("ajoutjoueur");
+    console.log(IdElement.value);
+
+    let ligne = document.createElement('tr');
+    let cellule = document.createElement('td');
+
+    cellule.innerHTML = IdElement.value;
+    ligne.appendChild(cellule);
+
+    let Idtable = document.getElementById("body");
+
+    Idtable.appendChild(ligne);
+  }
+  {
+    event.preventDefault();
+    let IdElement = document.getElementById("ajoutclub");
+    console.log(IdElement.value);
+
+    let ligne = document.createElement('tr');
+    let cellule = document.createElement('td');
+
+    cellule.innerHTML = IdElement.value;
+    ligne.appendChild(cellule);
+
+    let Idtable = document.getElementById("body");
+
+    Idtable.appendChild(ligne);
+  }
+  {
+    event.preventDefault();
+    let IdElement = document.getElementById("ajoutpays");
+    console.log(IdElement.value); let ligne = document.createElement('tr');
+    let cellule = document.createElement('td');
+
+    cellule.innerHTML = IdElement.value;
+    ligne.appendChild(cellule);
+
+    let Idtable = document.getElementById("body");
+
+    Idtable.appendChild(ligne);
+  }
 }
 
 /*localstorage avec données alignées*/
 const local = JSON.parse(localStorage.getItem("stocker"));
 
-if(local != null);
-    bouton.onclick = () => {
-      const stocker ={
-        rang: ajoutrang.value,
-        classement: ajoutclassementO.value,
-        points: ajoutpoints.value,
-        joueur: ajoutjoueur.value,
-        club: ajoutclub.value,
-        pays: ajoutpays.value
-      }  
-    localStorage.setItem("stocker",JSON.stringify(stocker));
- }
+if (local != null);
+bouton.onclick = () => {
+  const stocker = {
+    rang: ajoutrang.value,
+    classement: ajoutclassementO.value,
+    points: ajoutpoints.value,
+    joueur: ajoutjoueur.value,
+    club: ajoutclub.value,
+    pays: ajoutpays.value
+  }
+  localStorage.setItem("stocker", JSON.stringify(stocker));
+}
+
 
 
 
---
+++ scores.js
@@ -0,0 +1,20 @@
+/*flouter*/
+$( "#jA" ).first().click(function() {
+    $( this ).fadeTo( "slow", 0.33 );
+  });
+$( "#jB" ).first().click(function() {
+    $( this ).fadeTo( "slow", 0.33 );
+  }); 
+
+$( "#b1").first().click(function() {
+    $( '#jA').fadeTo( "slow", 0.3 );
+  });   
+$( "#b1").first().click(function() {
+    $( '#jB').fadeTo( "slow", 0.3 );
+  });   
+$( "#b2").first().click(function() {
+    $( '#jA').fadeTo( "slow", 1 );
+  });   
+$( "#b2").first().click(function() {
+    $( '#jB').fadeTo( "slow", 1 );
+  });   
