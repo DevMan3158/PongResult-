4c457aca7f3d6c01b842fd3c24f1ad2c6d59b123
Laetitia Gonin <l.gonin@codeur.online>
2022-08-04 15:44:22 +0200

    graphique en barres poue le top 5. réussi.

--- scores.js
+++ scores.js
@@ -18,3 +18,44 @@ $( "#b2").first().click(function() {
 $( "#b2").first().click(function() {
     $( '#jB').fadeTo( "slow", 1 );
   });   
+
+/*graphique*/
+const graph = document.getElementById("graph").getContext("2d");
+
+let myChart = new Chart(graph, {
+    type: "horizontalBar",
+    data: {
+        labels: [
+            "JoueurA",
+            "JoueurB",
+            "JoueurC",
+            "JoueurD",
+            "JoueurE",
+    ],
+    datasets: [
+        {
+        label:"Score",
+        data: [215, 212, 156, 132, 129],
+        backgroundColor: [
+            "purple",
+            "red",
+            "pink",
+            "orange",
+            "yellow",
+        ],
+        hoverBorderWidth: 3,
+        },
+    ],
+    },
+    options:{
+        title: {
+            display: true,
+            text: "Les 5 joueurs au podium",
+            fontSize: 25,
+        },
+    legend: {
+        display: false,
+    }   
+    }
+});
+
