0c8dd2244194ff2ce4e3f9c0852830febf6884c5
DevMan3158 <e.giner@codeur.online>
2022-08-04 17:54:02 +0200

    Modal Cookies

---
+++ javaScript/dialog.js
@@ -0,0 +1,31 @@
+let favDialog = document.getElementById('favDialog');
+let selectEl = document.querySelector('select');
+let updateButton = document.getElementById('modifier');
+let confirmBtn = document.getElementById('confirmer');
+let outputBox = document.querySelector('output');
+
+
+
+// Le bouton "Mettre à jour les détails" ouvre le <dialogue> ; modulaire
+
+updateButton.addEventListener('click', function onOpen() 
+{
+  if (typeof favDialog.showModal === "function") {
+    favDialog.showModal();
+
+  } else {
+    console.error("L'API <dialog> n'est pas prise en charge par ce navigateur.");
+  }
+});
+
+// L'entrée "Animal favori" définit la valeur du bouton d'envoi.
+selectEl.addEventListener('change', function onSelect(e) {
+  confirmBtn.value = selectEl.value;
+});
+
+
+// Le bouton "Confirmer" du formulaire déclenche la fermeture
+// de la boîte de dialogue en raison de [method="dialog"]
+/*favDialog.addEventListener('close', function onClose() {
+  outputBox.value = favDialog.returnValue + " bouton cliqué - " + (new Date()).toString();
+});*/
